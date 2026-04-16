f9be08f00b599456aa5c40492cc082a93facf212
azmainofficial <mdd518969@gmail.com>
2026-04-16 15:45:37 +0600

    Add js_class1 folder

---
+++ js_class1/main.js
@@ -0,0 +1,13 @@
+const member ={
+    img : "../sub-buzz-8947-1717257505-1.webp",
+    badge : "https://cdn.pixabay.com/photo/2016/03/31/19/56/medal-1293124_960_720.png",
+    name : "azmain",
+    deg : "CTO",
+    bio : "I`m a passionate web developer with expertise in front-end and back-end technologies. I love creating dynamic and responsive websites that provide an exceptional user experience.",
+    tech : ["HTML", "CSS", "JavaScript", "React", "Node.js"],
+    socialMeadia : {
+        github : "github.com/azmainofficial",
+        instagram : "https://www.instagram.com/hi_ho_ho_ho_/",
+        facebook : "https://www.facebook.com/profile.php?id=100009876543210"
+    }
+}
